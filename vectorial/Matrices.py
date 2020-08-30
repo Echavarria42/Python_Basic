d8f2c0c29cdac6ea5c2fbcf0eb812cd0fe397dee
Mateo Echavarria <echavarria.0042@gmail.com>
2020-08-30 10:38:38 -0500

    Traspaso class Matrices a Matrices.py

---
+++ vectorial/Matrices.py
@@ -0,0 +1,24 @@
+class Matriz:
+
+    def __init__(self, matriz):
+        self.dimencion = len(matriz)
+        self.matriz = matriz
+    
+    def sumar(self, matriz):
+        solution = [Vector(self.matriz[i]).sumar(  Vector(matriz.matriz[i])  ) for i in range(self.dimencion)]
+        return solution
+    
+    def restar(self, matriz):
+        solution = [Vector(self.matriz[i]).restar(  Vector(matriz.matriz[i])  ) for i in range(self.dimencion)]
+        return solution
+
+
+    def mul_escalar(self, escalar):
+        solution = [ Vector(i).mul_escalar(escalar) for i in self.matriz]
+        return solution  
+
+if __name__ == '__main__':
+    
+    m_1 = Matriz([[1,1,1],[1,1,1],[1,1,1]])
+    m_2 = Matriz([[2,2,2],[2,2,2],[2,2,2]])
+    m_3 = Matriz([[3,3,3],[3,3,3],[3,3,3]])
--- vectorial/Vectores.py
+++ vectorial/Vectores.py
@@ -26,33 +26,11 @@ class Vector:
         solution = [self.vector[i] * escalar for i in self.vector]
         return solution
 
-class Matriz:
 
-    def __init__(self, matriz):
-        self.dimencion = len(matriz)
-        self.matriz = matriz
-    
-    def sumar(self, matriz):
-        solution = [Vector(self.matriz[i]).sumar(  Vector(matriz.matriz[i])  ) for i in range(self.dimencion)]
-        return solution
-    
-    def restar(self, matriz):
-        solution = [Vector(self.matriz[i]).restar(  Vector(matriz.matriz[i])  ) for i in range(self.dimencion)]
-        return solution
-
-
-    def mul_escalar(self, escalar):
-        solution = [ Vector(i).mul_escalar(escalar) for i in self.matriz]
-        return solution  
 
 if __name__ == '__main__':
     v_1 = Vector([1,1,1])
     v_2 = Vector([2,2,2])
     v_3 = Vector([3,3,3])
 
-    m_1 = Matriz([[1,1,1],[1,1,1],[1,1,1]])
-    m_2 = Matriz([[2,2,2],[2,2,2],[2,2,2]])
-    m_3 = Matriz([[3,3,3],[3,3,3],[3,3,3]])
-
-    print(m_1.mul_escalar(5))
 
